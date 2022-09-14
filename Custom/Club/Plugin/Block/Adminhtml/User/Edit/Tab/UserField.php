5dfbc284ba3e84d927df6c4a6c8e878e4f7e9d5f
Sy Luu <syluu152@gmail.com>
2022-09-14 19:08:34 +0700

    Init Project

--- Custom/Club/Plugin/Block/Adminhtml/User/Edit/Tab/UserField.php
+++ Custom/Club/Plugin/Block/Adminhtml/User/Edit/Tab/UserField.php
@@ -9,18 +9,24 @@ class UserField
 
     protected $_systemStore;
 
+    protected $registry;
 
     public function __construct(\Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Store\Model\System\Store $systemStore)
+        \Magento\Store\Model\System\Store $systemStore,
+        \Magento\Framework\Registry $registry)
     {
         $this->storeManager = $storeManager;
         $this->_systemStore = $systemStore;
+        $this->registry = $registry;
     }
 
     public function aroundGetFormHtml(
         \Magento\User\Block\User\Edit\Tab\Main $subject,
         \Closure $proceed)
     {
+        $model = $this->registry->registry('permissions_user');
+        $data = $model->getData();
+
         $form = $subject->getForm();
         if (is_object($form))
         {
@@ -33,7 +39,8 @@ class UserField
                     'label' => __('Store'),
                     'title' => __('Store'),
                     'values' => $this->_systemStore->getStoreValuesForForm(false, false),
-                    'class' => 'select'
+                    'class' => 'select',
+                    'value' => isset($data['store_id']) ? $data['store_id'] : 0
 //                    'options' => $this->storeManager->getStore()->getName()
                 ]
             );
